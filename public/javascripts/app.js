dfa9caf85545f73b530c296064f1fc3ed80977cf
Matt Schemmel <matt.schemmel@gmail.com>
2011-06-04 21:14:47 -0700

    Beginning style updates

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -13,6 +13,7 @@ var $_ = $_ || {};
     var renderer;
 
   // CONSTANTS
+    var US_CENTER = new google.maps.LatLng(37.3002752813443, -93.33984375);
     var SEATTLE = new google.maps.LatLng(47.6063889, -122.3308333);
     var DEFAULT_ORIGIN = SEATTLE;
     var DEFAULT_DESTINATION = new google.maps.LatLng(47.61079236060622, -122.34237670898438);
@@ -28,7 +29,7 @@ var $_ = $_ || {};
     function log ( message ) {
         if(console) { console.log(message); }
     }
-  
+
     function toggleControls() {
         var controlBodyID = '#controls > .bd';
         var controlBody = $(controlBodyID);
@@ -40,14 +41,14 @@ var $_ = $_ || {};
         if ( isVisible ) {
             controlBody.slideUp(100, function() {
                // On completion of the slide-up, flip the image
-               $('#toggle-controls').html('down'); 
+                $('#toggle-button').removeClass("open").addClass("closed");
             });
             
         } else {
             // Animate the opening.
             controlBody.slideDown(100, function() {
                 // On completion of the slide-down, flip the image.
-                $('#toggle-controls').html('up');
+                $('#toggle-button').removeClass("closed").addClass("open");
             });
         }
     }
@@ -66,23 +67,67 @@ var $_ = $_ || {};
         $(el).removeClass("status-ok");
         $(el).addClass("status-error");
     }
+    
+    function closeHelpDialogs() {
+        $("a[rel='boxy']").each(function(idx, el) {
+            Boxy.linkedTo(el).hide();
+        });
+    }
 
     function clearResolvedAddress ( ) {
         // Strip the cached 'location' value from the 
-        $(this).removeData(ATTR_LOCATION);
+        $(this).closest("div.address").removeData(ATTR_LOCATION);
+    }
+    
+    function recordLocation ( el, result ) {
+        clearError(el);
+
+        var location = result.geometry.location;
+        
+        // single valid address - expected case
+        $(el).data(ATTR_LOCATION, location);
+    }
+    
+    function resolvePlace ( el, text, lock ) {
+        var placesService = new google.maps.places.PlacesService(map);
+        
+        var request = {
+            bounds: map.getBounds(),
+            name: text
+        }
+        
+        placesService.search ( request, function ( results, status ) {
+            if ( results.length == 1 ) {
+                log("Single result returned. Using as is.");
+                
+                recordLocation ( el, results[0] );
+            } else if ( results.length ) {
+                log("Multiple results returned. Using first result." );
+                
+                recordLocation ( el, results[0] );
+            
+            } else {
+                displayError ( el, "Unable to find lat/long coordinates for this address. Please reenter.");
+            }
+
+            // If we were given a deferred lock, resolve it now.            
+            if ( lock ) {
+                lock.resolve();
+            }
+        });
     }
     
     /**
      * Resolves the given address
      * @param {Object} addressEl
      */
-    function resolveAddress ( els ) {
+    function resolveAddress ( els, boundSearch ) {
         var lock = $.Deferred();
         
         if ( els.length ) {
             var el = els[0];
             
-            if ( $(el).hasData(ATTR_LOCATION) ) {
+            if ( $(el).data(ATTR_LOCATION) ) {
                 // Skip resolution - cached value remains valid. See the
                 //  'change' field for the address for details.
                 log("Skipping location retrieval for unchanged text field.");
@@ -93,30 +138,37 @@ var $_ = $_ || {};
                 var address = $("input", el).val(); // text field containing the address
                 
                 if ( address ) {
+                    var geocodeOpts = {
+                        "address": address
+                    };
+                    
+                    if(boundSearch) {
+                        geocodeOpts.bounds = map.getBounds();
+                    }
+                    
                     // Text found. Resolve the address.
-                    geocoder.geocode ( {"address": address}, function(results, status) {
+                    geocoder.geocode ( geocodeOpts, function(results, status) {
                         if ( results.length === 1 ) {
-                            clearError(el);
-    
-                            var result = results[0];
-                            var location = result.geometry.location;
+                            recordLocation(el, results[0]);
                             
-                            // single valid address - expected case
-                            $(el).data(ATTR_LOCATION, location);
-                            $("span.resolution", el).html(result.formatted_address);
-                            
+                            lock.resolve();
+
                         } else if ( results.length ) {
-                            // multiple possible addresses.
-                            displayError ( el, "This address matches multiple locations. Please be more specific.");
-                            $("span.resolution", el).html("");
+                            // multiple possible addresses. use the first
+                            var result = results[0];
                             
+                            recordLocation(el, results[0]);
+                            
+                            lock.resolve();
+
                         } else {
                             // no valid results found.
-                            displayError ( el, "Unable to find lat/long coordinates for this address. Please reenter.");
-                            $("span.resolution", el).html("");
+                            log ( "Unable to find a street address matching the input. Attempting to match as a Place instead.");
+                            
+                            // Try to interpret the value as a Place name instead.
+                            // Do NOT resolve the lock here - that is up to the places api.
+                            resolvePlace ( el, address, lock );
                         }
-                        
-                        lock.resolve();
                     });
                 } else {
                     // Simply resolve the lock - no resolution to be done.
@@ -128,7 +180,7 @@ var $_ = $_ || {};
         return lock.promise();
     }
     
-    function getBrowserGeolocation() {
+    function geolocate ( callback ) {
         // Read and cache module-global variable for geolocated value.
         // Note that a 'null' value indicates that we've previously tried
         //  and failed to geolocate through the browser. "undefined" is true
@@ -138,18 +190,32 @@ var $_ = $_ || {};
             
             // Ensure the browser supports W3C geolocation...
             if ( navigator.geolocation ) {
-                log ( "Browser supports geolocation... attemting to determine.");
+                log ( "Browser supports geolocation... attempting to determine.");
                 
-                navigator.geolocation.getCurrentPosition(function(pos) {
+                var deferred = $.Deferred();
+                navigator.geolocation.getCurrentPosition(function(pos){
+                    log("Browser supports geolocation; result determined.");
                     browserGeolocation = pos;
+                    deferred.resolve();
                     
-                }, function() {
-                    alert ( "Browser supports geolocation, but we were unable to determine the current location.");
+                }, function(){
+                    log("Browser supports geolocation, but we were unable to determine the current location.");
+                    deferred.reject();
+                }, {
+                    "timeout": 5000
                 });
+                
+                if (callback) {
+                    $.when(deferred.promise()).then(callback, callback);
+                }
+            }
+        } else {
+            // Trigger the callback immediately, as the browser geolocation is
+            //  already discovered and cached.
+            if ( callback ) {
+                callback.call();
             }
         }
-
-        return browserGeolocation;
     }
     
     function getOrigin() {
@@ -157,13 +223,13 @@ var $_ = $_ || {};
 
         // Use the entered address, if available.
         var requestedOrigin = $("div.address.origin").data(ATTR_LOCATION);
-        var geolocation = getBrowserGeolocation();
-        
+        geolocate();
+       
 	    if ( requestedOrigin) {
             result = requestedOrigin;
 
-        } else if ( geolocation ) {
-            result = new google.maps.LatLng(geolocation.coords.latitude, geolocation.coords.longitude);
+        } else if ( browserGeolocation ) {
+            result = new google.maps.LatLng(browserGeolocation.coords.latitude, browserGeolocation.coords.longitude);
         }
 
         return result;
@@ -175,7 +241,7 @@ var $_ = $_ || {};
         return requestedDestination;
     }
 
-    function updateMap ( origin, destination ) {
+    function replaceRoute ( origin, destination ) {
         var directionsService = new google.maps.DirectionsService();
         var request = {
             "origin": origin,
@@ -187,7 +253,10 @@ var $_ = $_ || {};
             // Keep a module-private variable to the renderer so that
             //  future updates
             if (!renderer) {
-                renderer = new google.maps.DirectionsRenderer();
+                renderer = new google.maps.DirectionsRenderer({
+                    draggable: true,
+                    panel: $('#directions')[0]
+                });
                 renderer.setMap(map);
             }
             
@@ -201,18 +270,35 @@ var $_ = $_ || {};
     function geolocateCenter() {
         var center = null;
         
-        var geolocation = getBrowserGeolocation();
-        if ( geolocation ) {
-            var inputEls = $("input#city");
+        geolocate ( function() {
+            if ( browserGeolocation ) {
+                var inputEls = $("input#region");
+                
+                var address = browserGeolocation.address;
+                if (address) {
+                    inputEls.val(address.city + ", " + address.region + " " + address.postalCode);
+                
+                } else {
+                    inputEls.val ( "Automatically detected address")
+                    inputEls.addClass ( "automatic" );
+                    log("Unable to determine street address from browser geolocation.");
+                }
+                
+                var coords = browserGeolocation.coords || {};
+                if ( coords ) {
+                    center = new google.maps.LatLng(coords.latitude, coords.longitude)
+                    inputEls.data(ATTR_LOCATION, center);
+                    
+                    updateMap(center, 11);
+                
+                } else {
+                    log ( "Unable to determine coordinates from browser geolocation." );
+                }
             
-            var address = geolocation.address;
-            inputEls.val(address.city + ", " + address.region + " " + address.postalCode);
-
-            center = new google.maps.LatLng(geolocation.coords.latitude, geolocation.coords.longitude)
-            inputEls.data(ATTR_LOCATION, center);
-            
-            updateMap(center);
-        }
+            } else {
+                log ( "Unable to geolocate." );
+            }
+        });
     }
     
     /**
@@ -248,14 +334,13 @@ var $_ = $_ || {};
 
     function initMap ( center, zoomLevel ) {
         var myOptions = {
-            "zoom": zoomLevel || 14,
-            "center": center,
+            "zoom": zoomLevel || 5,
+            "center": US_CENTER,
             "mapTypeId": google.maps.MapTypeId.ROADMAP
         };
         var mapCanvas = $("#map-canvas")[0];
     
         map = new google.maps.Map(mapCanvas, myOptions);
-        updateMap(center, zoomLevel);
 
         var bikeLayer = new google.maps.BicyclingLayer();
         bikeLayer.setMap(map);
@@ -263,34 +348,109 @@ var $_ = $_ || {};
     
     function initialize(){
         var restored = restoreSession();
-        var center = initContext();
-        initMap ( center );
+        initMap ( );
+        initContext();
+    }
+    
+    /**
+     * Performs a geocoding and updates the map center
+     */
+    function geocodeCenter() {
+        var els = $("div.address.region");
+        
+        $.when(resolveAddress(els, false)).then(function() {
+            var center = els.data(ATTR_LOCATION);
+            
+            if ( center ) {
+                updateMap(center, 11);
+                
+            } else {
+                log("Unable to resolve center.");
+            }
+        });
+        
+        // Always return false to prevent form submission
+        return false;
     }
     
     $_.update = function() {
         $.when(
-                resolveAddress ( $("div.address.origin") ),
-                resolveAddress ( $("div.address.destination") )
+                resolveAddress ( $("div.address.origin"), true ),
+                resolveAddress ( $("div.address.destination"), true )
         ).then ( function() {
             var origin = getOrigin();
             var destination = getDestination();
             
             if ( origin && destination ) {
-                updateMap ( origin, destination );
+                replaceRoute ( origin, destination );
+
+                // If we've added a new route, collapse the control bar.
+                toggleControls();
             }
         });
+        
+        return false; // Disable default form submission
+    }
+    
+    function submitSelf() {
+        $(this).closest("form").submit();
     }
 
-  // PERFORM INITIALIZATION ON THE MAIN PAGE
-    initialize();
+    function submitIfModified() {
+        if ( $(this).data(ATTR_LOCATION) ) {
+            log ( "Skipping update for already-recorded value." );
         
-  // SET UP EVENT HANDLERS
-    // Register 'click' handler to update the map.
-    $("input#update").click($_.update);
-    // Register 'change' handlers to clear data caches on text change.
-    $("input.address[type^='text']").change(clearResolvedAddress);
-    // Register 'click' handler to toggle the controls.
-    $('#toggle-controls').click(toggleControls);
-    // Register 'click' handler to geolocate a new map center.
-    $('#detect-city').click(geolocateCenter);
+        } else {
+            submitSelf.apply(this, arguments);
+            
+        }
+    }
+    
+    function _submit ( fn ) {
+        return function() {
+            // Apply the given method to the object, passing any additional arguments
+            fn.apply ( this, arguments );
+            
+            // Skip the function result; always return false.
+            return false;
+        }
+    } 
+    
+    $().ready(function() {
+      // PERFORM INITIALIZATION ON THE MAIN PAGE
+        initialize();
+            
+      // SET UP EVENT HANDLERS
+        // Register 'submit' handler to update the region selection
+        $("form#region-form").submit(_submit(geocodeCenter));
+      
+        // Register 'submit' handler to update the map.
+        $("form#control-form").submit(_submit($_.update));
+        $("form#control-form").validate({
+            messages: {
+                destination: {
+                   required: "Please enter a destination address."
+                }
+            }
+        });
+        
+        // Register 'change' handlers to clear data caches on text change.
+        $("input.address[type^='text']").change(clearResolvedAddress);
+        // Register 'click' handler to toggle the controls.
+        $('#toggle-controls').click(toggleControls);
+        
+        // Register 'click' handler to geolocate a new map center.
+        $('#detect-region').click(geolocateCenter).click(closeHelpDialogs);
+        // Register a 'blur' handler to geocode a new map center if the user manually enters text.
+        $('input#region').blur(submitIfModified);
+        
+        // Usability tweak. Make sure that all text is autoselected when any
+        //  input box receives the focus.
+        $('input').focus(function(){this.select()});
+        
+        // Register dialog triggers
+        $("a[rel='boxy']").boxy({
+            draggable: true
+        });
+    })
 })();
